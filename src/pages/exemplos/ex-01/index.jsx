ba7689a36e95670c99ce44bb35f3370339604241
mrotoliluiz-alt <mrotoliluiz@gmail.com>
2026-03-10 10:55:26 -0300

    aula 10/03
    home do "site"

---
+++ src/pages/exemplos/ex-01/index.jsx
@@ -0,0 +1,11 @@
+function Exemplo01() {
+    return (
+        <>
+            <h1>Exemplo 1</h1>
+            <p>Comnponente basico para renderizar tela com react</p>
+        </>
+    )
+}
+
+export default Exemplo01;
+
