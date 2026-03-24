9ae3b9cabca35e17d2249dfe99433b45d3cde75e
mrotoliluiz-alt <mrotoliluiz@gmail.com>
2026-03-24 10:56:14 -0300

    arrumei só kakakkaka

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Exemplo01 from './pages/exemplos/ex-01';
 import Exemplo02 from './pages/exemplos/ex-02';
+import Atividade01 from "./pages/atividades/atv-01";
 
 function App() {
   return (
